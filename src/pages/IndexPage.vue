a091a3d43325decf22dbb0ed1ecefedaced87a32
cgsalva <ascolong@gmail.com>
2024-10-20 13:20:06 -0600

    Se modico MainLayout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,25 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="q-pa-md example-row-equal-width">
+    <h2 class="text-weight-light text-center">Análisis de Calidad del Agua</h2>
+    <h4 class="text-weight-light text-center">Proyecto de Arquitectura de Computadoras 2024</h4>
+    
+    <div class="text-center">
+      <q-btn class="q-mx-xs" icon="autorenew" color="primary" label="Iniciar analisis" />
+      <q-btn class="q-mx-xs" icon="group" color="primary" label="Integrantes" />
+    </div>
+  </div>
 </template>
 
+<!--
+
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+import BarChart from '../components/GraficaPrueba.vue'
+//import PolarAreaChart from '../components/GraficaPrueba2.vue'
+
+/*
+export default {
+  name: 'App',
+  components: { BarChart }
+}*/
 </script>
+-->
